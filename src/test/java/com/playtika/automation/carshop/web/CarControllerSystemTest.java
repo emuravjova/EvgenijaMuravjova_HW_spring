20db8f6412e5404fbc8a15859feeaafbb3b16f15
emuravjova <emuravjova@playtika.com>
2017-11-14 15:15:37 +0200

    8th HW: system tests

--- src/test/java/com/playtika/automation/carshop/web/CarControllerSystemTest.java
+++ src/test/java/com/playtika/automation/carshop/web/CarControllerSystemTest.java
@@ -1,23 +1,19 @@
 package com.playtika.automation.carshop.web;
 
 import com.playtika.automation.carshop.domain.Car;
+import com.playtika.automation.carshop.domain.CarSaleDetails;
+import io.restassured.path.json.JsonPath;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.context.WebApplicationContext;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CarControllerSystemTest {
-
-    @Autowired
-    private WebApplicationContext context;
 
     @Test
     public void shouldAddCar() throws Exception {
@@ -38,15 +34,51 @@ public class CarControllerSystemTest {
     @Test
     public void shouldGetCarDetails() throws Exception {
         Car car = new Car("BMW","2010");
-        int id = given()
+        long id = given()
                 .contentType("application/json")
                 .body(car)
                 .when().post("/cars?price=25000&contacts=Bob 0969876543")
-                .andReturn().getBody().jsonPath().get("id");
+                .andReturn().getBody()
+                .as(CarSaleDetails.class).getId();
+
         given()
-                .when().get("/cars/{id}",id).then()
+                .when().get("/cars/{id}",id)
+                .then()
                 .body("contacts",equalTo("Bob 0969876543"))
                 .body("price",equalTo(25000))
                 .statusCode(200);
+    }
+
+    @Test
+    public void shouldGetCars() throws Exception {
+        Car car = new Car("BMW","2010");
+        long id = given()
+                .contentType("application/json")
+                .body(car)
+                .when().post("/cars?price=25000&contacts=Bob 0969876543")
+                .andReturn().getBody()
+                .as(CarSaleDetails.class).getId();
+
+        JsonPath jsonResponse = given()
+                .when().get("/cars").jsonPath();
+
+                assert(jsonResponse.get("find { it.id == "+ id +" }.car.name").equals(car.getName()));
+                assert(jsonResponse.get("find { it.id == "+ id +" }.car.model").equals(car.getModel()));
+                assert(jsonResponse.get("find { it.id == "+ id +" }.contacts").equals("Bob 0969876543"));
+                assert(jsonResponse.get("find { it.id == "+ id +" }.price").equals(25000));
+    }
+
+    @Test
+    public void shouldDeleteCar() throws Exception {
+        Car car = new Car("BMW","2010");
+        long id = given()
+                .contentType("application/json")
+                .body(car)
+                .when().post("/cars?price=25000&contacts=Bob 0969876543")
+                .andReturn().getBody()
+                .as(CarSaleDetails.class).getId();
+
+        given().when().delete("/cars/{id}",id).then().statusCode(200);
+        given().when().get("/cars").then().assertThat().body("id",not(hasValue(id)));
     }
 }
