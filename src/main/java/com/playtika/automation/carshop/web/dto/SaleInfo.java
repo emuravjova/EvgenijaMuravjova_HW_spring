818c8f1d3f323f21008ebd097e6dbb450e6d229a
emuravjova <emuravjova@playtika.com>
2017-11-16 21:06:15 +0200

    8th HW: fixes

---
+++ src/main/java/com/playtika/automation/carshop/web/dto/SaleInfo.java
@@ -0,0 +1,14 @@
+package com.playtika.automation.carshop.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by emuravjova on 11/16/2017.
+ */
+@Data
+@AllArgsConstructor
+public class SaleInfo {
+    private int price;
+    private String contacts;
+}
