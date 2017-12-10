ec8edcf6b1e463687a5f8487f54e7d00e6999f27
emuravjova <jane308pr@gmail.com>
2017-12-10 21:47:18 +0200

    11th HW: couchbase

--- src/main/java/com/playtika/automation/carshop/CarshopApplication.java
+++ src/main/java/com/playtika/automation/carshop/CarshopApplication.java
@@ -1,8 +1,16 @@
 package com.playtika.automation.carshop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication(exclude={WebSocketAutoConfiguration.class})
 public class CarshopApplication {
@@ -10,4 +18,5 @@ public class CarshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarshopApplication.class, args);
 	}
+
 }
