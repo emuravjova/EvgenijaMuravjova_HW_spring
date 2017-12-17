b9c911e65338709382836faeecf0040f5d9e55c3
emuravjova <jane308pr@gmail.com>
2017-12-17 23:41:40 +0200

    11th HW: couchbase disable

--- src/main/java/com/playtika/automation/carshop/dao/couchbase/CouchbaseOfferDao.java
+++ src/main/java/com/playtika/automation/carshop/dao/couchbase/CouchbaseOfferDao.java
@@ -1,16 +1,16 @@
-package com.playtika.automation.carshop.dao.couchbase;
-
-import com.playtika.automation.carshop.dao.OfferDao;
-import com.playtika.automation.carshop.dao.entity.OfferEntity;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.couchbase.core.query.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-/**
- * Created by emuravjova on 12/11/2017.
- */
-public interface CouchbaseOfferDao extends OfferDao {
-
-}
+//package com.playtika.automation.carshop.dao.couchbase;
+//
+//import com.playtika.automation.carshop.dao.OfferDao;
+//import com.playtika.automation.carshop.dao.entity.OfferEntity;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.data.couchbase.core.query.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+///**
+// * Created by emuravjova on 12/11/2017.
+// */
+//public interface CouchbaseOfferDao extends OfferDao {
+//
+//}
--- src/main/java/com/playtika/automation/carshop/dao/couchbase/CouchbaseSellerDao.java
+++ src/main/java/com/playtika/automation/carshop/dao/couchbase/CouchbaseSellerDao.java
@@ -1,17 +1,17 @@
-package com.playtika.automation.carshop.dao.couchbase;
-
-import com.playtika.automation.carshop.dao.SellerDao;
-import com.playtika.automation.carshop.dao.entity.SellerEntity;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.couchbase.core.query.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-/**
- * Created by emuravjova on 12/11/2017.
- */
-
-public interface CouchbaseSellerDao extends SellerDao {
-
-}
+//package com.playtika.automation.carshop.dao.couchbase;
+//
+//import com.playtika.automation.carshop.dao.SellerDao;
+//import com.playtika.automation.carshop.dao.entity.SellerEntity;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.data.couchbase.core.query.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+///**
+// * Created by emuravjova on 12/11/2017.
+// */
+//
+//public interface CouchbaseSellerDao extends SellerDao {
+//
+//}
